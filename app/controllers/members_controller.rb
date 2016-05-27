f1432ee8699c96f9808d7442e63e79bb0b616c40
Rebecca Appleyard <rklappleyard@gmail.com>
2016-05-27 16:17:49 +0100

    Playing around with eager loading

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,10 +2,9 @@ class MembersController < ApplicationController
 
 	def index
 		uri = "http://data.parliament.uk/resource/#{params[:concept_id]}"
-		#concept = Concept.find(uri)
-		question = Question.where(subjectId: uri)
-		render :text => question
-		#@members = concept.questions.map{ |q| q.tablingMember }.uniq
+		concept = Concept.find(uri)
+		concept.eager_load_object_triples!
+		@members = concept.questions.map{ |q| q.tablingMember }.uniq
 	end
 
 end
